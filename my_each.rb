a541ee8c68f2eb07881bb31bbf83d98f7061e99c
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 04:31:46 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arguements)
+  counter = 0
+  while arguements.length > counter 
+  yield arguements[counter]
+  counter += 1
+end
+arguements
 end
